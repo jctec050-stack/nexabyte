2b85afe4e4e8fb1ebe0cb3ac57b3cd2876e1c749
Juan Piris <jctec050@gmail.com>
2025-12-18 12:58:54 -0300

    Add floating WhatsApp button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"
 import Navbar from "@/components/layout/Navbar"
 import Footer from "@/components/layout/Footer"
+import WhatsAppButton from "@/components/ui/WhatsAppButton"
 import Home from "@/pages/Home"
 import Services from "@/pages/Services"
 import Contact from "@/pages/Contact"
@@ -30,6 +31,7 @@ function App() {
           </Routes>
         </main>
         <Footer />
+        <WhatsAppButton />
       </div>
     </Router>
   )
